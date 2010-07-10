efbb19a68eac13216afad60a3c796182ff6f82fd
techtangents <dylan@techtangents.com>
2010-07-11 00:50:49 +1000

    Removed double validation.

--- src/main/java/com/techtangents/oneof/core/value/OneOfValueFactory.java
+++ src/main/java/com/techtangents/oneof/core/value/OneOfValueFactory.java
@@ -4,14 +4,13 @@ import com.techtangents.oneof.api.DefaultOne;
 import com.techtangents.oneof.types.value.OneOf;
 
 import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Proxy;
+
+import static java.lang.reflect.Proxy.newProxyInstance;
 
 public class OneOfValueFactory {
-    private final Validator validator = new Validator();
 
     public Object make(Object o, Class<? extends OneOf> cls, final Class... clarses) {
-        validator.validateObject(o, clarses);
         InvocationHandler i = new OneOfInvocationHandler(o, clarses);
-        return Proxy.newProxyInstance(DefaultOne.class.getClassLoader(), new Class[]{cls}, i);
+        return newProxyInstance(DefaultOne.class.getClassLoader(), new Class[]{cls}, i);
     }
 }
